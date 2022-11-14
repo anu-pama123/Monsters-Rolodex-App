b097f0fc559cf5c94001399384bada8234ab5875
Anupama CV <anupamac@bipp.io>
2022-11-14 17:41:01 +0530

    Added: class component

--- monsters-rolodex-app/src/App.js
+++ monsters-rolodex-app/src/App.js
@@ -1,11 +1,44 @@
+import { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters : [
+        {
+          name : 'Linda',
+          id : '123ab'
+        },
+        {
+          name : 'Frank',
+          id : '123cd'
+        },
+        {
+          name : 'Jacky',
+          id : '123ef'
+        },
+        {
+          name : 'Andrei',
+          id : '123gh'
+        }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+       {
+        this.state.monsters.map((monster) => {
+          return <h1 key = {monster.id}>{monster.name}</h1>
+        })
+       }
+      </div>
+    );
+  }
 }
 
 export default App;
